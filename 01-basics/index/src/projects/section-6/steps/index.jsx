a6f473f5e5e6564c52d0231be4c75ebc1fdc1839
Diego Martins <diego.martins@vilt-group.com>
2026-01-19 18:26:04 -0300

    steps

--- 01-basics/index/src/projects/section-6/steps/index.jsx
+++ 01-basics/index/src/projects/section-6/steps/index.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import "./styles.css";
 
 const messages = [
@@ -8,7 +8,15 @@ const messages = [
 ];
 
 function Steps() {
-	const step = 1;
+	const [step, setStep] = useState(1);
+
+	const handleNext = () => {
+		step < 3 && setStep((prevStep) => prevStep + 1);
+	};
+
+	const handlePrevious = () => {
+		step > 1 && setStep((prevStep) => prevStep - 1);
+	};
 
 	return (
 		<>
@@ -25,14 +33,16 @@ function Steps() {
 				</div>
 				<div className="buttons">
 					<button
-						className="previous"
+						className={`previous ${step === 1 ? "inactive" : ""}`}
 						style={{ backgroundColor: "#7950f2", color: "#fff" }}
+						onClick={() => handlePrevious()}
 					>
 						Previous
 					</button>
 					<button
-						className="next"
+						className={`next ${step === 3 ? "inactive" : ""}`}
 						style={{ backgroundColor: "#7950f2", color: "#fff" }}
+						onClick={() => handleNext()}
 					>
 						Next
 					</button>
