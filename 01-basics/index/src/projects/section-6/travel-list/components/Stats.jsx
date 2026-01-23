57cea97b54015a307e2ad70ec3f27fdddaca8965
Diego Martins <diego.martins@vilt-group.com>
2026-01-23 14:26:36 -0300

    far away finished

--- 01-basics/index/src/projects/section-6/travel-list/components/Stats.jsx
+++ 01-basics/index/src/projects/section-6/travel-list/components/Stats.jsx
@@ -1,9 +1,27 @@
 import React from "react";
 
-function Stats() {
+function Stats({ items }) {
+	if (!items.length)
+		return (
+			<p className="stats">
+				<em>Star adding items to your packing list</em>
+			</p>
+		);
+
+	const totalItems = items.length;
+	const itemsPacked = items.filter((item) => item.packed).length;
+	const percentage = Math.round((itemsPacked / totalItems) * 100);
+
 	return (
 		<footer className="stats">
-			<em>💼 You have X items on your list, and you already packed X (X%)</em>
+			{percentage < 100 ? (
+				<em>
+					💼 You have {totalItems} items on your list, and you already packed{" "}
+					{itemsPacked}({percentage}%)
+				</em>
+			) : (
+				<em>You got everything! Ready to Go! 🛫</em>
+			)}
 		</footer>
 	);
 }
--- 01-basics/index/src/projects/section-6/travel-list/index.jsx
+++ 01-basics/index/src/projects/section-6/travel-list/index.jsx
@@ -34,7 +34,7 @@ function TravelList() {
 				onDeleteItem={handleDeleteItems}
 				onToggleItem={handleToggleItem}
 			/>
-			<Stats />
+			<Stats items={items} />
 		</div>
 	);
 }
