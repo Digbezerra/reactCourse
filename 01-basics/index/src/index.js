252726e08773f7f40cb081e911cf602487cfe21c
Diego Martins <diego.martins@vilt-group.com>
2026-01-30 16:13:03 -0300

    tabs

--- 01-basics/index/src/index.js
+++ 01-basics/index/src/index.js
@@ -15,13 +15,16 @@ import ReactDOM from "react-dom/client";
 
 //SECTION-8
 // import UsePopCorn from "./projects/section-8/use-pop-corn";
+// import TextExpanderApp from "./projects/section-8/text-expander";
 
-import TextExpanderApp from "./projects/section-8/text-expander";
+//SECTION-9
+
+import Tabs from "./projects/section-8/tabs";
 
 function App() {
 	return (
 		<>
-			<TextExpanderApp />
+			<Tabs />
 		</>
 	);
 }
