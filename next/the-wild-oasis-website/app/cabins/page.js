cd39518707c50e03ce1007d0ebaa834616b1e800
Diego Martins <diego.martins@vilt-group.com>
2026-02-04 15:47:34 -0300

    wild oasis initial structure

---
+++ next/the-wild-oasis-website/app/cabins/page.js
@@ -0,0 +1,15 @@
+export default async function Page() {
+	const res = await fetch("https://jsonplaceholder.typicode.com/users");
+	const data = await res.json();
+
+	return (
+		<div>
+			<h1>Cabins page</h1>
+			<ul>
+				{data.map((data) => {
+					return <li key={data.id}>{data.name}</li>;
+				})}
+			</ul>
+		</div>
+	);
+}
---
+++ next/the-wild-oasis-website/app/page.js
@@ -0,0 +1,7 @@
+export default function Home() {
+	return (
+		<div>
+			<h1>The wild Oasis, welcome to paradise</h1>
+		</div>
+	);
+}
