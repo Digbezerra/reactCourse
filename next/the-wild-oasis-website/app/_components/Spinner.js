98500e97b8ae1479507b7c91956d62e5ffb521d7
Diego Martins <diego.martins@vilt-group.com>
2026-02-09 14:38:45 -0300

    error handling, routers

--- next/the-wild-oasis-website/app/_components/Spinner.js
+++ next/the-wild-oasis-website/app/_components/Spinner.js
@@ -1,5 +1,10 @@
-function Spinner() {
-  return <div className='spinner'></div>;
+function Spinner({ pageName = "" }) {
+	return (
+		<div className="grid items-center justify-center">
+			<span className="spinner"></span>
+			<p className="text-xl text-primary-200">Loading {pageName} data...</p>
+		</div>
+	);
 }
 
 export default Spinner;
