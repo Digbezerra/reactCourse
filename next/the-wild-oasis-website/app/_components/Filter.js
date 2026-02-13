54cbc86aec16fec9315dc44fb3f27a5bd0dd3724
Diego Martins <diego.martins@vilt-group.com>
2026-02-13 18:12:45 -0300

    next

--- next/the-wild-oasis-website/app/_components/Filter.js
+++ next/the-wild-oasis-website/app/_components/Filter.js
@@ -1,29 +1,61 @@
 "use client";
 
+import { usePathname, useRouter, useSearchParams } from "next/navigation";
+
 export default function Filter() {
-	function handleFilter(filter) {}
+	const searchParams = useSearchParams();
+	const router = useRouter();
+	const pathname = usePathname();
+
+	const activeFilter = searchParams.get("capacity") ?? "all";
+
+	function handleFilter(filter) {
+		const params = new URLSearchParams(searchParams);
+		params.set("capacity", filter);
+		router.replace(`${pathname}?${params.toString()}`, { scroll: false });
+	}
 
 	return (
 		<div className="border border-primary-800 flex">
-			<button className="px-5 py-2 hover:bg-primary-700">All cabins</button>
-			<button
-				className="px-5 py-2 hover:bg-primary-700"
-				onClick={() => handleFilter("small")}
+			<FilterButton
+				filter="all"
+				activeFilter={activeFilter}
+				handleFilter={handleFilter}
+			>
+				All Cabins
+			</FilterButton>
+			<FilterButton
+				filter="small"
+				activeFilter={activeFilter}
+				handleFilter={handleFilter}
 			>
 				1&mdash;3 guests
-			</button>
-			<button
-				className="px-5 py-2 hover:bg-primary-700"
-				onClick={() => handleFilter("medium")}
+			</FilterButton>
+			<FilterButton
+				filter="medium"
+				activeFilter={activeFilter}
+				handleFilter={handleFilter}
 			>
 				4&mdash;7 guests
-			</button>
-			<button
-				className="px-5 py-2 hover:bg-primary-700"
-				onClick={() => handleFilter("large")}
+			</FilterButton>
+			<FilterButton
+				filter="large"
+				activeFilter={activeFilter}
+				handleFilter={handleFilter}
 			>
 				8&mdash;12 guests
-			</button>
+			</FilterButton>
 		</div>
+	);
+}
+
+function FilterButton({ activeFilter, filter, handleFilter, children }) {
+	return (
+		<button
+			className={`px-5 py-2 hover:bg-primary-700 ${filter === activeFilter ? "bg-primary-700 text-primary-50" : ""}`}
+			onClick={() => handleFilter(filter)}
+		>
+			{children}
+		</button>
 	);
 }
---
+++ next/the-wild-oasis-website/app/about/loading.js
@@ -0,0 +1,8 @@
+export default function Loading() {
+	return (
+		<div className="grid items-center justify-center">
+			<span className="spinner"></span>
+			<p className="text-xl text-primary-200">Loading data...</p>
+		</div>
+	);
+}
